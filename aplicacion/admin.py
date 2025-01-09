a44c9daee0cccb1c4d6e9423c276f258167d40fb
JosCarRub <josecarcatarubio6@gmail.com>
2025-01-09 17:51:08 +0100

    Initial commit

---
+++ aplicacion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Oferta, TipoDeOferta
+
+# Register your models here.
+admin.site.register(Oferta)
+admin.site.register(TipoDeOferta)
---
+++ aplicacion/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.forms import ValidationError
+
+# Create your models here.
+
+class Oferta(models.Model):
+    PLATAFORMA_OPCIONES = [
+                ('tiktok', 'TikTok' ),
+                ('ig', 'Instagram' ),
+                ('fb', 'Facebook' ),
+                ('otra', 'Otra' ),
+    ]
+
+    ESTADO_OPCIONES = [
+        ('vig', 'Vigente' ),
+        ('exp', 'Expirada' ),
+    ]
+    #ID_OFERTA
+    #empresa_id = models.ForeigKey[...]
+
+    descripcion = models.TextField()
+    requisitos = models.TextField()
+    
+
+    plataformas = models.CharField(max_length=50, choices = PLATAFORMA_OPCIONES)
+    otra_plataforma = models.CharField(max_length=100,
+                                        blank=True,  null=True )
+    
+    fecha_publicacion = models.DateField()
+    fecha_expiracion = models.DateField()
+
+    
+    
+
+class TipoDeOferta(models.Model):
+
+    OFERTA_OPCIONES = [
+                ('gastro', 'Gastronomía' ),
+                ('moda', 'Moda' ),
+                ('hogar', 'Hogar' ),
+                ('tecnologia', 'Tecnología' ),
+                ('eventos', 'Eventos' ),
+                ('otra', 'Otro' ),
+    ]
+
+    oferta_id = models.ForeignKey(Oferta, on_delete=models.CASCADE, related_name="ofertas")
+
+    tipo = models.CharField(max_length=100, choices=OFERTA_OPCIONES, verbose_name='Tipo de oferta')
+
+    class Meta:
+        verbose_name = 'Tipo de oferta'
+        verbose_name_plural = 'Tipos de ofertas'
+
+    def __init__(self):
+        return f'Tipo de oferta: {self.tipo}'
+
+class RangoPuntuacion(models.IntegerChoices):
+    MUY_MALA = 1, 'Muy mala'
+    MALA = 2, 'Mala'
+    REGULAR = 3, 'Regular'
+    BUENA = 4, 'Buena'
+    EXCELENTE = 5, 'Excelente'
+    
+class AplicacionOferta(models.Model):
+
+    ESTADO_APLICACION_OFERTA = [
+        ('aceptada', 'Aceptada'),
+        ('denegada', 'Denegada'),
+        ('solicitada', 'Solicitada'),
+    ]
+
+    #usuario_id
+    oferta_id = models.ForeignKey(Oferta, on_delete=models.CASCADE, related_name="ofertas")
+    estado_aplicacion = models.CharField(max_length=100, choices=ESTADO_APLICACION_OFERTA, verbose_name='Estado de la aplicación a la oferta')
+    fecha_expiracion = models.DateField()
+    puntuacion = models.IntegerChoices(RangoPuntuacion.choices, verbose_name = 'Puntuación',
+                                        null=True,  
+                                        blank=True,  
+                                        )
+
+
+    
+    #FUNCION CLEAN PARA VALIDAR QUE SI ESTÁ LA OFERTA HECHA SE PUEDE PUNTUAR
+
+
+    class Meta:
+        verbose_name = 'Aplicación a la oferta'
+        verbose_name_plural = 'Aplicaciones a las ofertas'
+
+    def __init__(self):
+        return f'Estado de la oferta: {self.estado_aplicacion}'
