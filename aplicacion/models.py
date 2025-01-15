31c0fbe290591707fe9222959e61adf00854df82
JosCarRub <josecarcatarubio6@gmail.com>
2025-01-15 10:15:40 +0100

    css templates and login

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.forms import ValidationError
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#class Usuario(User):
+    #nombre = models.CharField(max_length=50, verbose_name= 'Nombre de usuario')
+    #email = models.EmailField()
+    #preguntar JI
+
 
 class Oferta(models.Model):
     PLATAFORMA_OPCIONES = [
