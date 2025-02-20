7da790332a14fac6415a5e38a2ca309f21c0f797
JosCarRub <josecarcatarubio6@gmail.com>
2025-02-20 13:12:19 +0100

    cosas

--- proyecto_personal/settings/__init__.py
+++ proyecto_personal/settings/__init__.py
@@ -3,6 +3,6 @@ import os
 ENV = os.getenv('DJANGO_ENV', 'production')  # Cambiar esta variable en producción
 
 if ENV == 'production':
-    from .local import *
-else:
     from .production import *
+else:
+    from .local import *
